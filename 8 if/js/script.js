f221699febb2de828ea155a8ef97c9ad150f3bb6
Arseq <arseqpage@gmail.com>
2020-11-30 20:52:59 +0300

    if

---
+++ 8 if/js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// if(4) {
+//   console.log('OK');
+// } else {
+//   console.log('No ok');
+// }
+
+// const nav = 50;
+
+// if(nav < 49) {
+//   console.log('Err');
+// } else if (nav > 100) {
+//   console.log('Big');
+// } else {
+//   console.log('Ok');
+// }
+
+// (nav === 50) ? console.log('Ok') : console.log('Error');
+
+const num = 50;
+
+switch (num) {
+  case 49:
+    console.log('False');
+    break;
+  case 100:
+    console.log('False');
+    break;
+  case 20:
+      console.log('True');
+      break;
+  default:
+    console.log('Koker');
+    break;
+    
+}
