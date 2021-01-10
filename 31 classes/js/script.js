06068271cb14e2b3f7980fc6db139af0e20c0e40
Arseq <arseqpage@gmail.com>
2021-01-10 21:57:29 +0300

    classes

--- 31 classes/js/script.js
+++ 31 classes/js/script.js
@@ -1,26 +1,35 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
 
-number = 10;
-
-console.log(number);
-
-const obj = {
-  a: 50,
-};
-
-obj.a = 10;
-
-console.log(obj);
-
-console.log(name);
-
-var name = "Ivan";
-
-{
-  let result = 33;
+  calcArea() {
+    return this.height * this.width;
+  }
 }
 
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
+  }
 
+  showMyProps() {
+    console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+  }
+}
+
+// const square = new Rectangle(34, 10),
+//   long = new Rectangle(122, 10);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+div.showMyProps();
+
+console.log(div.calcArea());
